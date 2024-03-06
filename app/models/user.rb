0f0f0e77811ea99031bb15e9669b6e3ba9aabaae
CJeanne ANonym <anonyme201504@gmail.com>
2024-03-06 11:08:49 -0300

    hey

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def self.find_for_database_authentication(conditions={})
+      find_by(username: conditions[:email]) || find_by(email: conditions[:email])
+  end
 end
