c07a00d04b0e6e47c9bd908734427a032ee07577
CJeanne ANonym <anonyme201504@gmail.com>
2024-03-07 11:59:50 -0300

    hey

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_06_142621) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
